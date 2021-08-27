1145753ecd7871374cfd0ebaaa8411629f23a013
zhongji <featherfly@foxmail.com>
2021-08-27 17:46:06 +0800

    test

---
+++ src/test/java/cn/featherfly/conversion/BasicTest3.java
@@ -0,0 +1,31 @@
+
+package cn.featherfly.conversion;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import cn.featherfly.common.lang.reflect.GenericClass;
+import cn.featherfly.conversion.string.ToStringBeanPropertyConversion;
+import cn.featherfly.conversion.string.ToStringConversionPolicy;
+import cn.featherfly.conversion.string.ToStringConversionPolicys;
+
+/**
+ * @author 钟冀
+ */
+public class BasicTest3 {
+
+    ToStringConversionPolicy policy;
+
+    @BeforeClass
+    public void setUp() {
+        policy = ToStringConversionPolicys.getFormatConversionPolicy();
+    }
+
+    @Test
+    public void test1() {
+        ToStringBeanPropertyConversion c = new ToStringBeanPropertyConversion(policy);
+
+        System.out.println(c.targetToSource("123456", new GenericClass<>(Long.class)));
+
+    }
+}
